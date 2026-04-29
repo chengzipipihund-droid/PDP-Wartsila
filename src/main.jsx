df86c9d80f68951e54f86d47a337bfd8e1d20480
Keyu-1218 <benchu1218@gmail.com>
2026-04-29 15:59:51 +0300

    feat: config for vercel portfolio deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './pages/energy/App.jsx'
 import AlarmApp from './pages/alarm/AlarmApp.jsx'
 import MobileApp from './pages/alarm/MobileApp.jsx'
@@ -8,16 +8,25 @@ import NavPage from './pages/nav/NavPage.jsx'
 import PortfolioPage from './Pages/portfolio/PortfolioPage.jsx'
 import './index.css'
 
+const isPortfolioOnly = import.meta.env.VITE_PORTFOLIO_ONLY === 'true'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/"       element={<App />} />
-        <Route path="/alarm"  element={<AlarmApp />} />
-        <Route path="/nav"    element={<NavPage />} />
-        <Route path="/mobile" element={<MobileApp />} />
-        <Route path="/portfolio" element={<PortfolioPage />} />
-      </Routes>
+      {isPortfolioOnly ? (
+        <Routes>
+          <Route path="/" element={<PortfolioPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/"       element={<App />} />
+          <Route path="/alarm"  element={<AlarmApp />} />
+          <Route path="/nav"    element={<NavPage />} />
+          <Route path="/mobile" element={<MobileApp />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+        </Routes>
+      )}
     </BrowserRouter>
   </React.StrictMode>,
 )
